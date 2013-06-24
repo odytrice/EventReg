aa24892a9f89645b199c7805fd09e5c9a2752162
Ody Mbegbu <odytrice@gmail.com>
2013-06-24 10:18:23 +0100

    Initial Load - Created Project From Angular Seed - Added Bootstrap, Angular, Underscore...

---
+++ scripts/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BASE_DIR=`dirname $0`
+
+echo ""
+echo "Starting Karma Server (http://karma-runner.github.io)"
+echo "-------------------------------------------------------------------"
+
+karma start $BASE_DIR/../config/karma.conf.js $*
+
+read -p "Press [Enter] key to start backup..."
