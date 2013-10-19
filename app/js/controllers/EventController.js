df9b10a0332f56c58daaebb297280d464b5cf0cc
Ody Mbegbu <odytrice@gmail.com>
2013-10-19 21:10:49 +0100

    Chapter 2.1

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,15 +3,38 @@
 eventsApp.controller('EventController',
     function ($scope) {
         $scope.event = {
-            name:"Angular Boot Camp",
+            name: "Angular Boot Camp",
             date: '1/1/2013',
             time: '10:30 am',
-            location:{
-                address:'Google Headquarters',
-                city:'Mountain View',
+            location: {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
                 province: 'CA'
             },
-            imageUrl: '/img/angularjs-logo.png'
+            imageUrl: '/img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: "Directives Masterclass",
+                    creatorName: "Bob Smith",
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'In this session you will learn the ins and outs of directives'
+                },
+                {
+                    name: "Scopes for fun and profit",
+                    creatorName: 'John Doe',
+                    duration: '30 mins',
+                    level: 'introductory',
+                    abstract: 'This session will take a closer look at scopes, Learn what they do, how they do it, and how to get them to do it for you.'
+                },
+                {
+                    name: "Well behaved Controllers",
+                    creatorName: "Jane Doe",
+                    duration: "2 Hours",
+                    level: "Intermediate",
+                    abstract: "Controllers are the beginning of everything Angular does. Learn how to craft controllers that will win the respect of your friends and neighbours."
+                }
+            ]
         }
     }
 );
